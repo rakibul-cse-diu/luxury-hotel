ae305f45ea0a32a435b98350b0feafc67ea7d5c1
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-19 18:43:33 +0600

    add rooms

--- src/components/Facilities/Facilities.js
+++ src/components/Facilities/Facilities.js
@@ -20,44 +20,44 @@ const Facilities = () => {
             </div>
             <div className='flex flex-col justify-center items-center my-14'>
                 <div className='flex flex-col justify-center items-center mb-12'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={gym} alt="" />
-                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[23%] md:left-[33%] lg:left-[40%]'>The Gym</h2>
+                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[23%] md:left-[33%] lg:left-[39%]'>The Gym</h2>
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center items-center my-12'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={bar} alt="" />
-                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[14%] md:left-[26%] lg:left-[36%]'>Pool Side Bar</h2>
+                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[14%] md:left-[26%] lg:left-[34%]'>Pool Side Bar</h2>
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center items-center my-12'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={spa} alt="" />
                         <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[23%] md:left-[33%] lg:left-[40%]'>The Spa</h2>
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center items-center my-5'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={pool} alt="" />
-                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[10%] md:left-[23%] lg:left-[35%]'>Swimming Pool</h2>
+                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[10%] md:left-[23%] lg:left-[33%]'>Swimming Pool</h2>
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center items-center my-5'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={restuarant} alt="" />
-                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[18%] md:left-[30%] lg:left-[37%]'>Restuarant</h2>
+                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[18%] md:left-[30%] lg:left-[36%]'>Restuarant</h2>
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center items-center my-5'>
-                    <div className='flex flex-col relative w-3/4 md:w-2/4'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 hover:scale-105 duration-700'>
                         <img className='w-full' src={laundry} alt="" />
-                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[23%] md:left-[33%] lg:left-[39%]'>Laundry</h2>
+                        <h2 className='text-xl px-10 rounded py-1 font-bold bg-white text-black absolute bottom-0 left-[23%] md:left-[33%] lg:left-[38%]'>Laundry</h2>
                     </div>
                 </div>
             </div>
--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Testimonial from '../Testimonial/Testimonial';
+import single from '../../images/single-room.png';
+import double from '../../images/double-room.png';
+import twin from '../../images/twin-room.png';
 
 const Rooms = () => {
     return (
         <div>
             <Header headerClass='rooms-header' />
-            <h2>THis is Rooms Page...</h2>
+            <h2 className='text-3xl font-bold text-center mt-8'>Rooms</h2>
+            <div className='flex flex-col justify-center items-center mt-5'>
+                <p className='font-light w-3/4 text-center'>Each of our bright, light-flooded rooms come with everything you could possibly need for a comfortable stay. And yes,
+                    comfort isn't our only objective, we also value good design, sleek contemporary furnishing complemented
+                    by the rich tones of nature's palette as visible from our rooms' sea-view windows and terraces. </p>
+            </div>
+            <div className='flex flex-col justify-center items-center my-14'>
+                <div className='flex flex-col justify-center items-center mb-12'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 rounded-b-[15px] border-[1px] border-[#14274A] hover:scale-105 duration-700'>
+                        <img className='w-full' src={single} alt="" />
+                        <h2 className='text-2xl text-center w-full py-2 font-bold bg-[#14274A] text-white relative bottom-0'>SINGLE ROOM</h2>
+                        <div className='px-16 py-14 grid grid-cols-2 gap-5'>
+                            <button className='px-4 py-2 text-lg font-semibold duration-700 text-[#14274A]'>VIEW ROOM DETAILS</button>
+                            <button className='px-4 py-2 text-lg rounded-[3px] font-semibold bg-orange-300 duration-700 hover:bg-orange-400 text-black'>$147  Avg/night</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex flex-col justify-center items-center my-12'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 rounded-b-[15px] border-[1px] border-[#14274A] hover:scale-105 duration-700'>
+                        <img className='w-full' src={double} alt="" />
+                        <h2 className='text-2xl text-center w-full py-2 font-bold bg-[#14274A] text-white relative bottom-0'>DOUBLE ROOM</h2>
+                        <div className='px-16 py-14 grid grid-cols-2 gap-5'>
+                            <button className='px-4 py-2 text-lg font-semibold duration-700 text-[#14274A]'>VIEW ROOM DETAILS</button>
+                            <button className='px-4 py-2 text-lg rounded-[3px] font-semibold bg-orange-300 duration-700 hover:bg-orange-400 text-black'>$147  Avg/night</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex flex-col justify-center items-center my-12'>
+                    <div className='flex flex-col relative w-3/4 md:w-2/4 rounded-b-[15px] border-[1px] border-[#14274A] hover:scale-105 duration-700'>
+                        <img className='w-full' src={twin} alt="" />
+                        <h2 className='text-2xl text-center w-full py-2 font-bold bg-[#14274A] text-white relative bottom-0'>TWIN ROOM</h2>
+                        <div className='px-16 py-14 grid grid-cols-2 gap-5'>
+                            <button className='px-4 py-2 text-lg font-semibold duration-700 text-[#14274A]'>VIEW ROOM DETAILS</button>
+                            <button className='px-4 py-2 text-lg rounded-[3px] font-semibold bg-orange-300 duration-700 hover:bg-orange-400 text-black'>$147  Avg/night</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Testimonial />
         </div>
     );
 };
